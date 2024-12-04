6dff7a2ff51236ff761c432eb804bb718b5c2f45
nyashbox <nyashbox@tutanota.com>
2024-12-04 05:40:57 +0200

    build: build project with CMake

---
+++ src/core/stub.cc
@@ -0,0 +1,3 @@
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 nyashbox and Contributors
---
+++ tests/stub.test.cc
@@ -0,0 +1,8 @@
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 nyashbox and Contributors
+
+#include <gtest/gtest.h>
+
+// Run stub test
+TEST(stub, StubTest) { EXPECT_TRUE(true); }
