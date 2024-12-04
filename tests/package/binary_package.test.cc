8f541c858ca212b21893794c146d1e8856762b59
nyashbox <nyashbox@tutanota.com>
2024-12-04 19:29:09 +0200

    test: add test stubs for package classes

---
+++ tests/package/binary_package.test.cc
@@ -0,0 +1,9 @@
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 nyashbox and Contributors
+
+#include <gtest/gtest.h>
+
+#include "package/binary_package.h"
+
+TEST(BinaryPackage, stub) { GTEST_SKIP_("Not implemented"); }
---
+++ tests/package/source_package.test.cc
@@ -0,0 +1,10 @@
+
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 nyashbox and Contributors
+
+#include <gtest/gtest.h>
+
+#include "package/source_package.h"
+
+TEST(SourcePackage, stub) { GTEST_SKIP_("Not implemented"); }
