bee3e897419b65a7dd9f834d80eaad5fc924c07b
nyashbox <nyashbox@tutanota.com>
2024-12-04 19:25:37 +0200

    feat: add PackageBase class declaration

---
+++ src/package/package_base.h
@@ -0,0 +1,22 @@
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 nyashbox and Contributors
+
+#ifndef UKPM_PACKAGE_BASE_H_
+#define UKPM_PACKAGE_BASE_H_
+
+#pragma once
+
+namespace ukpm {
+namespace package {
+
+class PackageBase {
+  public:
+  protected:
+  private:
+};
+
+} // namespace package
+} // namespace ukpm
+
+#endif // UKPM_PACKAGE_BASE_H_
