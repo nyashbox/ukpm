9d77c673b70e173b3ad3654490bcabfc5c8194cb
nyashbox <nyashbox@tutanota.com>
2024-12-04 19:26:57 +0200

    feat: add BinaryPackage class declaration

---
+++ src/package/binary_package.cc
@@ -0,0 +1,9 @@
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 nyashbox and Contributors
+
+#include "package/binary_package.h"
+
+namespace ukpm {
+namespace package {} // namespace package
+} // namespace ukpm
---
+++ src/package/binary_package.h
@@ -0,0 +1,24 @@
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 nyashbox and Contributors
+
+#ifndef UKPM_PACKAGE_BINARY_H_
+#define UKPM_PACKAGE_BINARY_H_
+
+#pragma once
+
+#include "package/package_base.h"
+
+namespace ukpm {
+namespace package {
+
+class BinaryPackage final : PackageBase {
+  public:
+  protected:
+  private:
+};
+
+} // namespace package
+} // namespace ukpm
+
+#endif // UKPM_PACKAGE_BINARY_H_
