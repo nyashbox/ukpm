e3c5e2a285ee34df9fdde1eeb6272d37cd693275
nyashbox <nyashbox@tutanota.com>
2024-12-04 19:27:27 +0200

    feat: add SourcePackage class declaration

---
+++ src/package/source_package.cc
@@ -0,0 +1,9 @@
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 nyashbox and Contributors
+
+#include "package/source_package.h"
+
+namespace ukpm {
+namespace package {} // namespace package
+} // namespace ukpm
