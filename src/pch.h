1f5001234bac9cb31632fe8453420f8275f0bfbc
nyashbox <nyashbox@tutanota.com>
2024-12-16 17:51:51 +0200

    feat: add header precompilation

---
+++ src/pch.h
@@ -0,0 +1,19 @@
+
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 nyashbox and Contributors
+
+#ifndef UKPM_PCH_H_
+#define UKPM_PCH_H_
+
+// C++ standard library
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
+
+// json
+#include <jsoncons/json.hpp>
+#include <jsoncons_ext/jsonschema/json_schema_factory.hpp>
+#include <jsoncons_ext/jsonschema/jsonschema.hpp>
+
+#endif // UKPM_PCH_H_
